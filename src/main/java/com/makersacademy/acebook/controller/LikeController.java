7d6b3788d278cc27789021fd8ce9f06b4bc97eb7
Joe <josephshaw1563@gmail.com>
2021-11-08 15:42:57 +0000

    errors

--- src/main/java/com/makersacademy/acebook/controller/LikeController.java
+++ src/main/java/com/makersacademy/acebook/controller/LikeController.java
@@ -1,16 +1,32 @@
 package com.makersacademy.acebook.controller;
 
+import com.makersacademy.acebook.model.Like;
+import com.makersacademy.acebook.repository.LikeRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class LikeController {
+  @Autowired
+    LikeRepository likeRepository;
 
   @GetMapping("/posts")
   public RedirectView index() {
 		return new RedirectView("/posts");
+  }
+
+    @PostMapping("/posts")
+    public RedirectView create(@ModelAttribute Like Like) {
+        
+        return new RedirectView("/posts");
+    }
+
   
 
 
