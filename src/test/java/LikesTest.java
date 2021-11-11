9816b1aab0914a42ba77331c99ed164f1f83f4fe
Joe <josephshaw1563@gmail.com>
2021-11-11 16:03:37 +0000

    almost there:

--- src/test/java/LikesTest.java
+++ src/test/java/LikesTest.java
@@ -17,5 +17,50 @@ import org.openqa.selenium.WebElement;
 @SpringBootTest(classes = Application.class)
 public class LikesTest {
 
+  WebDriver driver;
+  Faker faker;
+  public String name;
+
+
+  @Before 
+  public void setup(){
+    System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
+    driver = new ChromeDriver();
+    faker = new Faker();
+
+    name = faker.name().firstName();
+    signup(name, "password");
+    login(name, "password"); 
+    post("Hello");
+  }
+
+  
+
+  private void signup(String username, String password){
+    driver.get("http://localhost:8080/users/new");
+    driver.findElement(By.id("username")).sendKeys(username);
+    driver.findElement(By.id("password")).sendKeys("password");
+    driver.findElement(By.id("submit")).click();
+  }
+  private void login(String username, String password){
+    driver.get("http://localhost:8080/login.html");
+    driver.findElement(By.id("username")).sendKeys(username);
+    driver.findElement(By.id("password")).sendKeys("password");
+    driver.findElement(By.cssSelector("input[type='submit'][value='Log in']")).click();
+  }
+  private void post(String content){
+    driver.findElement(By.id("content")).sendKeys(content);
+    driver.findElement(By.cssSelector("input[type='submit'][value='Post']")).click();
+  }
+
+  @After
+  public void teardown(){
+    driver.close();
+  }
+ @Test
+ public void userlikespost(){
+  driver.findElement(By.cssSelector("input[type='submit'][value='Like']")).click();
+  Assert.assertEquals(1,driver.findElement(By.cssSelector("")).getText());
+ }
 
  }
